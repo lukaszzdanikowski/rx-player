cf0ffaad3f3dfe410e84fc5bebb7dacd6673d6aa
Alexandre Duros <alexandre.duros@gmail.com>
2015-11-26 14:43:45 +0100

    added longer content

--- demo/bundle.js
+++ demo/bundle.js
@@ -31773,6 +31773,16 @@
 	    "mimeType": "application/bif",
 	    "url": "http://dash-vod-aka-test.canal-bis.com/test/bif/index.bif"
 	  }
+	}, {
+	  "name": "Grand Journal (bif)",
+	  "url": "http://hss-vod-aka-test.canal-bis.com/ondemand/test/bif2/index.ism/Manifest",
+	  "transport": "smooth",
+	  "ciphered": false,
+	  "live": false,
+	  "images": {
+	    "mimeType": "application/bif",
+	    "url": "http://dash-vod-aka-test.canal-bis.com/test/bif/index2.bif"
+	  }
 	}];
 
 /***/ }
--- demo/contents.js
+++ demo/contents.js
@@ -44,5 +44,16 @@ module.exports = [
       "mimeType": "application/bif",
       "url": "http://dash-vod-aka-test.canal-bis.com/test/bif/index.bif"
     }
+  },
+  {
+    "name": "Grand Journal (bif)",
+    "url": "http://hss-vod-aka-test.canal-bis.com/ondemand/test/bif2/index.ism/Manifest",
+    "transport": "smooth",
+    "ciphered": false,
+    "live": false,
+    "images": {
+      "mimeType": "application/bif",
+      "url": "http://dash-vod-aka-test.canal-bis.com/test/bif/index2.bif"
+    }
   }
 ];
